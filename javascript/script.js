60d44e24d945e0e0134adc0a14481f90366553f2
imgokulraj <r.gokul705@gmail.com>
2023-06-29 14:47:39 +0530

    ok commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,104 @@
 const swup = new Swup();
+let currentPageName = null
+const articles = [
+    {
+        "img" : "/images/traversy.webp" , 
+        "size" : "large" , 
+        "title" : "Traversy Media" , 
+        "para" : "Hello Everyone" , 
+        "a" : '#' , 
+        "class" : "readings" ,
+        "classnum" : 1
+    },
+    {
+        "img" : "/images/deved.webp" , 
+        "size" : "large" , 
+        "title" : "Dev Ed" , 
+        "para" : "Hello Everyone",
+        "a" : '#',
+        "class" : "articles",
+        "classnum" : 2
+    },
+    {
+        "img" : "/images/techleead.webp" , 
+        "size" : "small" , 
+        "title" : "tech lead" , 
+        "para" : "Hello Everyone",
+        "a" : '#',
+        "class" : "videos",
+        "classnum" : 3
+    },
+    {
+        "img" : "/images/traversy.webp" , 
+        "size" : "medium" , 
+        "title" : "Traversy Media" , 
+        "para" : "Hello Everyone",
+        "a" : '#',
+        "class" : "videos" ,
+        "classnum" : 3
+    }
+    
+]
 
+projects = [
+    {
+        "id" : 1 ,
+        "img" : "/images/project1.gif" , 
+        "size" : "large" , 
+        "title" : "Piggment" , 
+        "para" : "The gradients of the summer of the next creator" , 
+        "a" : 'https://www.google.com' , 
+        "class" : "Projects" ,
+        "classnum" : 1 , 
+        "about" : "Some content" , 
+        "website" : "https://www.google.com" , 
+        "github" : "https://github.com/imgokulraj",
+        "technology" : ["javascript" , "python"]
+    },
+    {
+        "id" : 2,
+        "img" : "/images/project2.webp" , 
+        "size" : "medium" , 
+        "title" : "Piggment2" , 
+        "para" : "The gradients of the summer of the next creator" , 
+        "a" : '#' , 
+        "class" : "Projects" ,
+        "classnum" : 1 , 
+        "about" : "Some content 2" , 
+        "website" : "https://www.google.com" , 
+        "github" : "https://github.com/imgokulraj",
+        "technology" : ["javascript" , "python"]
+    },
+    {
+        "id" : 3,
+        "img" : "/images/project3.gif" , 
+        "size" : "small" , 
+        "title" : "shopper" , 
+        "para" : "The gradients of the summer of the next creator" , 
+        "a" : '#' , 
+        "class" : "Projects" ,
+        "classnum" : 1 , 
+        "about" : "Some content 3" , 
+        "website" : "https://www.google.com" , 
+        "github" : "https://github.com/imgokulraj",
+        "technology" : ["javascript" , "python"]
+    },
+    {
+        "id" : 4 ,
+        "img" : "/images/project4.webp" , 
+        "size" : "small" , 
+        "title" : "Piggment" , 
+        "para" : "The gradients of the summer of the next creator" , 
+        "a" : '#' , 
+        "class" : "Projects" ,
+        "classnum" : 1 , 
+        "about" : "Some content 4" , 
+        "website" : "https://www.google.com" , 
+        "github" : "https://github.com/imgokulraj",
+        "technology" : ["javascript" , "python"]
+    },
+    
+]
 swup.on("contentReplaced" , e => {
    loadCommonElements()
    console.log("changed")
@@ -54,10 +153,11 @@ document.querySelectorAll('a').forEach(aTag => {
 loadIndependentPageEvents() 
 }
 function loadIndependentPageEvents() {
-    const currentPageName = getCurrentPageName()
+    currentPageName = getCurrentPageName()
     if (currentPageName === 'index')loadJavascriptForIndexPage()
     else if(currentPageName === 'contact') loadJavascriptForContactPage()
     else if (currentPageName === 'articles') loadJavascriptForArticlesPage() 
+    else if (currentPageName === 'projects') loadJavascriptForProjectsPage()
 }
 function loadJavascriptForIndexPage(){
 // Redirecting to about page on clicking more about me and adding calm down animation
@@ -76,19 +176,114 @@ function loadJavascriptForContactPage() {
 function loadJavascriptForArticlesPage(){
     const navFilter = document.querySelector('.nav-filter')
     navFilter.addEventListener('click' , navFilterSelection)
+    populateContentImages(articles , document.querySelector('.articles-list') , 0)
+}
+function loadJavascriptForProjectsPage() {
+    const navFilter = document.querySelector('.nav-filter') 
+    navFilter.addEventListener('click' , navFilterSelection)
+    populateContentImages(projects , document.querySelector('.articles-list') , 0)
+    const projectItems = document.querySelector('.articles-list')
+    projectItems.addEventListener('click' , openProjectManual)
+    // event listenr to remove active modal
+    const openprojectmodal = document.querySelector('.project-details-modal')
+    openprojectmodal.addEventListener('click' , closeprojectdetailsmodal)
 }
 loadCommonElements()
-
+function openProjectManual(e) {
+    if(e.target.classList.contains("articles-article")){
+        const id = parseInt( e.target.getAttribute("id"))
+        openProjectDetails(id)
+    }
+}
+function closeprojectdetailsmodal(e) {
+    if (!e.target.classList.contains('back-to-projects')) return
+    e.target.innerHTML = '' 
+    document.querySelector('.project-details-modal').classList.remove("project-details-modal-show")
+    document.querySelector('.cover-whole-page-modal').classList.remove('cover-whole-page-modal-active')
+}
+function openProjectDetails(id) {
+    const obj = projects.filter(item => item.id === id)[0]
+    let technology = '' 
+    obj.technology.forEach(item => technology += `
+        <button>${item}</button>
+    `)
+    document.querySelector('.project-details-modal').innerHTML = `
+    <div class="back-btns">
+    <i class="fa-solid fa-arrow-left back-to-projects"></i>
+    <p class="back-to-projects">Back To Projects.</p>
+    </div>
+    <hr>
+    <h3>${obj.title}</h3>
+    <p>${obj.para}</p>
+    <img src="${obj.img}" alt="">
+    <h5>About</h5>
+    <p>${obj.about}</p>
+    <h5>Technologies</h5>
+    <div class="technology-btns">
+        ${technology}
+    </div>
+    <h5>Github</h5>
+    <a class="github" href="${obj.github}">${obj.github}</a>
+    <a class="open-project" href="${obj.website}">Open Project <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+    
+    `
+    // <a href="${obj.website}" class="open-projet">Open Project <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+    document.querySelector('.project-details-modal').classList.add("project-details-modal-show")
+    document.querySelector('.cover-whole-page-modal').classList.add('cover-whole-page-modal-active')
+}
 // Event listener functions 
 function navFilterSelection(e) {
 
     if (e.target.classList.contains("nav-filter-btn")) {
         if (e.target.classList.contains("nav-filter-btn-active")) return 
         const alreadySelectedBtn = document.querySelector('.nav-filter-btn-active') 
-        console.log(alreadySelectedBtn)
         if (alreadySelectedBtn !== null) alreadySelectedBtn.classList.remove('nav-filter-btn-active')
         e.target.classList.add("nav-filter-btn-active")
-
+        if (currentPageName === "projects") {
+            populateContentImages( projects , document.querySelector('.articles-list') , e.target.getAttribute("classnum"))
+        }else{
+            populateContentImages(articles , document.querySelector('.articles-list') , e.target.getAttribute("classnum"))
+        }
+        
         
     }
+}
+
+// populating content images 
+function populateContentImages(items , parent , filternum){
+    filternum = parseInt(filternum)
+    let newFilteredItems = items.filter( lst => lst.classnum === filternum)
+    if (filternum === 0) newFilteredItems = items
+    res = '<div class="left-column">' 
+    
+    for(let i = 0 ; i < Math.ceil(newFilteredItems.length / 2) ; i++) {
+        const obj = newFilteredItems[i] 
+        res += `
+            <li id="${obj.id}" class="articles-article ${obj.size}">
+                <img src="${obj.img}">
+                <div class="article-list-content">
+                    <h4>${obj.title}</h1>
+                    <p>${obj.para}</p>
+                </div>
+            </li>
+        `
+    }
+
+    res += '</div><div class="right-column">'
+    for(let i = Math.ceil(newFilteredItems.length / 2) ; i < newFilteredItems.length; i++) {
+        const obj = newFilteredItems[i] 
+        res += `
+            <li id="${obj.id}" class="articles-article ${obj.size}">
+                <img src="${obj.img}">
+                <div class="article-list-content">
+                    <h4>${obj.title}</h1>
+                        <p>${obj.para}</p>
+                </div>
+            </li>
+        `
+    }
+    res += '</div>' 
+    parent.classList.add("show")
+    parent.innerHTML = res
+
 }
